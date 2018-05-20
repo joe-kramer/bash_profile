a56cf13f94878980093424908d576536b159f020
Joe Kramer <joekramer@iSpots-MacBook-Pro-4.local>
2018-05-20 12:48:46 -0700

    update profile

--- scripts/download_profile.sh
+++ scripts/download_profile.sh
@@ -2,13 +2,12 @@
 # Created 05/20/2018
 # Update github repo
 
-TOP=$(pwd | xargs dirname)
+cd $(pwd | xargs dirname)
 
-cd $TOP
 git pull origin master
 
 rm ~/.bash_profile
-cat $TOP/bash_profile.txt >> ~/.bash_profile
+cat bash_profile.txt >> ~/.bash_profile
 source ~/.bash_profile
 
 echo done.

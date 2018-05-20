7b63c5fec4f5bdac406d704f58d06855e3bd5866
Joe Kramer <joekramer@iSpots-MacBook-Pro-4.local>
2018-05-20 12:45:01 -0700

    update profile

--- scripts/upload_profile.sh
+++ scripts/upload_profile.sh
@@ -2,13 +2,11 @@
 # Created 05/20/2018
 # Upload ~/.bash_profile to github
 
-TOP=$(pwd | xargs dirname)
+cd $(pwd | xargs dirname)
+rm bash_profile.txt
 
-ls $TOP
-rm $TOP/bash_profile.txt
-ls $TOP
-cat ~/.bash_profile >> $TOP/bash_profile.txt
-ls $TOP
+cat ~/.bash_profile >> bash_profile.txt
+
 git add .
 git commit -m "update profile"
 git push origin master
